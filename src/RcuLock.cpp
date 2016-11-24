0d0cb3af0e2fbc709b82d36eb8452a53d1677359
Javeme <javaloveme@gmail.com>
2016-11-24 20:00:19 +0800

    Add a read-write lock to protect member variables
    Add a read-write lock to protect member variables of class RcuLock.

--- src/RcuLock.cpp
+++ src/RcuLock.cpp
@@ -5,7 +5,8 @@ private:
 	volatile unsigned int m_epoch, m_writing;
 	Atomic m_currentReaders, m_nextReaders;
 	ConditionVar m_waiter;
-	Mutex m_mutex;
+	Mutex m_mutex; // for writers
+	SpinRWLock m_rwLock; // for writer and readers
 public:
 	RcuLock() : m_epoch(1), m_writing(0),
 		m_currentReaders(0), m_nextReaders(0)
@@ -22,9 +23,7 @@ public:
 		//     1.2.if writing != 0, m_epoch may be increased or not,
 		//       1.2.1.if m_epoch increased, thus next epoch, safe!
 		//      >1.2.2.if m_epoch not increased(unexpected state, be careful!),
-		//         but we can assume m_epoch increased, because m_epoch would
-		//         equal to m_writing finally, we just use(return) m_writing
-		//         instead of m_epoch, so this is safe!!
+		//         so we should add lock for this.
 		//   2.if moveNextToCurrentEpoch() is calling by write thread,
 		//     changed share variables are m_writing and readers,
 		//     2.1.if writing != 0, readers must be not moved, safe!
@@ -33,26 +32,26 @@ public:
 		//      >2.2.2.if not moved from m_nextReaders to m_currentReaders,
 		//         (unexpected state, be careful!)
 		//         we can't call m_currentReaders.increase(), NOT safe!!!
-		//         but we can wait until it's moved OK, so this is safe!!
+		//         so we should add lock for this.
 
-		unsigned int writing = m_writing;
-		smp_rmb();
+		m_rwLock.lockRead();
+
 		unsigned int epoch = this->currentEpoch();
 
 		// it is writing (into next epoch)
-		if(writing != 0) {
+		if(m_writing != 0) {
 			m_nextReaders.increase();
 			// should return the next epoch if writing
-			return writing;
+			epoch = m_writing;
 		}
 		// no writer now
 		else {
-			// spin until readers are moved OK if 2.2.2
-			while(m_writing == 0 && m_nextReaders > 0);
-
 			m_currentReaders.increase();
-			return epoch;
 		}
+
+		m_rwLock.unlockRead();
+
+		return epoch;
 	}
 
 	void readEnd(unsigned int epoch)
@@ -68,41 +67,29 @@ public:
 		//      >2.2.2.if not moved from m_nextReaders to m_currentReaders,
 		//         (unexpected state, be careful!)
 		//         we can't call m_currentReaders.decreaseAndTest(), NOT safe!!!
-		//         but we can wait until it's moved OK, so this is safe!!
+		//         so we should add lock for this.
+
+		m_rwLock.lockRead();
 
 		unsigned int writing = m_writing;
+
 		// it is writing (into next epoch) and `epoch` equals to the next epoch
-		if(epoch == writing) {
+		if(epoch == m_writing) {
 			assert(writing > 0);
 			// decrease number of next readers
-			// NOTE: m_nextReaders may be < 0 due to m_writing changed above,
-			//   like 1.writer set m_writing=0 and clear m_nextReaders;
-			//        2.wake up here, now m_nextReaders==0, -1 after decrease();
 			m_nextReaders.decrease();
-
-			// NOTE: m_writing may be changed before m_nextReaders.decrease():
-			//   non-zero => zero or non-zero => non-zero + 1.
-			if(writing != m_writing) {
-				// undo (but...how to deal with m_nextReaders=0 after moved OK?)
-				m_nextReaders.increase();
-				// once again
-				return readEnd(epoch);
-			}
 		}
 		// no writer now or `epoch` equals to the current epoch
 		else {
-			// spin until readers are moved OK if 2.2.2
-			while(m_writing == 0 && m_nextReaders > 0);
-
 			// decrease number of current readers and test if it's zero
-			// NOTE: this is correct even if m_writing is changed here:
-			//   zero => non-zero. because we should also decrease
-			//   m_currentReaders after non-writing => writing.
-			if(m_currentReaders.decreaseAndTest()) {
+			if(m_currentReaders.decrease() == 0) {
 				// notify waiter if there is no current readers
-				m_waiter.notify();
+				if(writing > 0)
+					m_waiter.notify();
 			}
 		}
+
+		m_rwLock.unlockRead();
 	}
 
 	// for writer
@@ -127,25 +114,28 @@ protected:
 		return m_epoch;
 	}
 
-	unsigned int nextEpoch()
+	int nextEpoch()
 	{
 		// we assume m_mutex is locked here
 		assert(m_mutex.locked());
 
+		m_rwLock.lockWrite();
+
 		// overflow, jump from 0 to 1 to avoid m_writing=0
 		if(m_epoch == -1) {
 			m_writing = 1;
-			smp_mb(); // to ensure change m_writing before m_epoch for read
-			m_epoch = 1;
 		}
-		// increase m_epoch and set m_writing for the next epoch readers
+		// set m_writing for the next epoch readers
 		else {
-			// NOTE: in fact we set m_writing to the next epoch (not current!)
+			// NOTE: in fact we set m_writing to next epoch (not current epoch!)
 			m_writing = m_epoch + 1;
-			smp_mb(); // to ensure change m_writing before m_epoch for read
-			++m_epoch;
 		}
-		return m_epoch;
+
+		int readers = m_currentReaders.load();
+
+		m_rwLock.unlockWrite();
+
+		return readers;
 	}
 
 	void moveNextToCurrentEpoch()
@@ -153,25 +143,20 @@ protected:
 		// we assume m_mutex is locked here
 		assert(m_mutex.locked());
 
-		// reset pending next epoch
+		m_rwLock.lockWrite();
+
+		// set next epoch to m_epoch and reset m_writing
+		m_epoch = m_writing;
 		m_writing = 0;
 
-		// mb for readBegin()/readEnd()
-		// to ensure change m_writing before m_nextReaders/m_currentReaders
-		smp_mb();
+		// get the number of next readers
+		int next = m_nextReaders;
+		// move `next` to m_currentReaders
+		m_nextReaders.sub(next);
+		m_currentReaders.add(next);
+		assert(m_nextReaders == 0);
 
-		while(true) {
-			// get the number of next readers
-			int next = m_nextReaders;
-			// move `next` to m_currentReaders if m_nextReaders not changed
-			// between above statement and next statement
-			if(m_nextReaders.subAndTest(next)) {
-				// NOTE: maybe m_currentReaders != 0 after set m_writing = 0,
-				//   so use add(next) instead of set(next)
-				m_currentReaders.add(next);
-				break;
-			}
-		}
+		m_rwLock.unlockWrite();
 	}
 
 	void waitForReaders()
@@ -179,17 +164,15 @@ protected:
 		// we assume m_mutex is locked here
 		assert(m_mutex.locked());
 
-		int current = this->currentEpoch();
-		int readers = m_currentReaders;
-
 		// increase epoch after me
-		this->nextEpoch();
+		int readers = this->nextEpoch();
 
 		// wait for current readers
 		if(readers > 0) {
 			//m_waiters[current].wait(m_mutex);
 			//m_waiters.remove(current);
-			m_waiter.wait(m_mutex);
+			// NOTE: m_currentReaders may be decreased before wait()
+			m_waiter.wait();
 			assert(m_currentReaders == 0);
 		}
 
@@ -199,6 +182,7 @@ protected:
 };
 
 // test RcuLock
+template <class Item>
 class RcuLockedList
 {
 private:
